72942c11a2f9b311bf4223ad032dba1e88708881
Anders Pitman <&ers@apitman.com>
2024-02-05 11:14:23 -0700

    Add file reading to demos

--- examples/simple_nonbrowser/index.js
+++ examples/simple_nonbrowser/index.js
@@ -1,5 +1,14 @@
 import { Client } from './lib/muxado-js/index.js';
 import { Server, parseRangeHeader } from './lib/http-js/index.js';
+import { openFile } from './lib/fs-js/index.js';
+
+const f = await openFile("./package.json");
+
+const dec = new TextDecoder('utf-8');
+
+for await (const chunk of f.readable) {
+  console.log(dec.decode(chunk));
+}
 
 const SERVER_DOMAIN = "anderspitman.net";
 const TUNNEL_DOMAIN = "test.anderspitman.net";
@@ -14,9 +23,13 @@ const httpServer = new Server({
   domain: TUNNEL_DOMAIN,
 });
 
+
 console.log("serve");
 httpServer.serve(session, async (r) => {
   console.log(r);
-  return new Response("Hi there", {
+
+  const f = await openFile("./package.json");
+
+  return new Response(f.readable, {
   });
 });
