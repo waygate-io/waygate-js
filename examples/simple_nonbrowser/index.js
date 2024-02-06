4447d30ddf7a65a58b2f2d96a0c72565c3f55bcc
Anders Pitman <&ers@apitman.com>
2024-02-06 11:22:46 -0700

    Update demo to use directoryTreeHandler

--- examples/simple_nonbrowser/index.js
+++ examples/simple_nonbrowser/index.js
@@ -1,14 +1,6 @@
 import { Client } from './lib/muxado-js/index.js';
-import { Server, parseRangeHeader } from './lib/http-js/index.js';
-import { openFile } from './lib/fs-js/index.js';
-
-const f = await openFile("./package.json");
-
-const dec = new TextDecoder('utf-8');
-
-for await (const chunk of f.readable) {
-  console.log(dec.decode(chunk));
-}
+import { Server, directoryTreeHandler, parseRangeHeader } from './lib/http-js/index.js';
+import { openFile, openDirectory } from './lib/fs-js/index.js';
 
 const SERVER_DOMAIN = "anderspitman.net";
 const TUNNEL_DOMAIN = "test.anderspitman.net";
@@ -23,13 +15,8 @@ const httpServer = new Server({
   domain: TUNNEL_DOMAIN,
 });
 
+const dir = await openDirectory(".");
+const handler = directoryTreeHandler(dir)
 
 console.log("serve");
-httpServer.serve(session, async (r) => {
-  console.log(r);
-
-  const f = await openFile("./package.json");
-
-  return new Response(f.readable, {
-  });
-});
+httpServer.serve(session, handler);
