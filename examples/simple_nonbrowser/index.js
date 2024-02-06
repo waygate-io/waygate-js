87e33fdb932ffd51c2129bd4dd5b31e21cc6cf03
Anders Pitman <&ers@apitman.com>
2024-02-06 11:27:28 -0700

    Clean up demo a bit

--- examples/simple_nonbrowser/index.js
+++ examples/simple_nonbrowser/index.js
@@ -1,22 +1,24 @@
-import { Client } from './lib/muxado-js/index.js';
-import { Server, directoryTreeHandler, parseRangeHeader } from './lib/http-js/index.js';
-import { openFile, openDirectory } from './lib/fs-js/index.js';
+import { Client as MuxadoClient } from './lib/muxado-js/index.js';
+import { Server as HttpServer, directoryTreeHandler } from './lib/http-js/index.js';
+import { openDirectory } from './lib/fs-js/index.js';
 
 const SERVER_DOMAIN = "anderspitman.net";
 const TUNNEL_DOMAIN = "test.anderspitman.net";
 
-const session = new Client({
+const muxSession = new MuxadoClient({
   serverDomain: SERVER_DOMAIN,
   tunnelDomain: TUNNEL_DOMAIN,
   token: "yolo",
 });
 
-const httpServer = new Server({
+const listener = muxSession;
+
+const server = new HttpServer({
   domain: TUNNEL_DOMAIN,
 });
 
 const dir = await openDirectory(".");
 const handler = directoryTreeHandler(dir)
 
-console.log("serve");
-httpServer.serve(session, handler);
+console.log("Running");
+server.serve(listener, handler);
