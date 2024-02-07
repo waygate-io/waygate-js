9bcedd989822b79f146a76788d905cd76e993efe
Anders Pitman <&ers@apitman.com>
2024-02-06 23:25:38 -0700

    Switch to async muxado client creation

--- examples/simple_nonbrowser/index.js
+++ examples/simple_nonbrowser/index.js
@@ -1,24 +1,27 @@
-import { Client as MuxadoClient } from './lib/muxado-js/index.js';
+import { connect } from './lib/muxado-js/index.js';
 import { Server as HttpServer, directoryTreeHandler } from './lib/http-js/index.js';
 import { openDirectory } from './lib/fs-js/index.js';
 
 const SERVER_DOMAIN = "anderspitman.net";
 const TUNNEL_DOMAIN = "test.anderspitman.net";
 
-const muxSession = new MuxadoClient({
-  serverDomain: SERVER_DOMAIN,
-  tunnelDomain: TUNNEL_DOMAIN,
-  token: "yolo",
-});
+(async () => {
 
-const listener = muxSession;
+  const muxSession = await connect({
+    serverDomain: SERVER_DOMAIN,
+    tunnelDomain: TUNNEL_DOMAIN,
+    token: "yolo",
+  });
 
-const server = new HttpServer({
-  domain: TUNNEL_DOMAIN,
-});
+  const listener = muxSession;
 
-const dir = await openDirectory(".");
-const handler = directoryTreeHandler(dir)
+  const server = new HttpServer({
+    domain: TUNNEL_DOMAIN,
+  });
 
-console.log("Running");
-server.serve(listener, handler);
+  const dir = await openDirectory(".");
+  const handler = directoryTreeHandler(dir)
+
+  console.log("Running");
+  server.serve(listener, handler);
+})();
